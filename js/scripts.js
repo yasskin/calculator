0a299fafe055e08bfdf05dc2d809c9c7e6bd227e
Alex Jacks and Noah Yasskin <noah@>
2016-05-31 14:29:17 -0700

    add responsive jQuery background color change

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,46 @@
 //Simple calculator code, addition and then subtraction of 2 input values//
-var Noah = function(num1, num2) {
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+		$("body").removeClass();
+		$("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+		$("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+		$("body").removeClass();
+    $("body").addClass("red-background");
+  });
+});
+
+
+
+/* Base calc code
+var Adam = function(num1, num2) {
 	return num1 + num2;
+};
+
+var Sunshine = function(num1, num2) {
+	return num1 - num2;
+};
+
+var Mike = function(num1, num2) {
+	return num1 * num2;
+};
+
+var Dave = function(num1, num2) {
+	return num1 / num2;
 };
 
 var num1 = parseInt(prompt("Gimme a number:"));
 var num2 = parseInt(prompt("Gimme another number!"));
 
-alert(Noah(num1, num2));
+alert(Adam(num1, num2));
+alert(Sunshine(num1, num2));
+alert(Mike(num1, num2));
+alert(Dave(num1, num2));
+*/
