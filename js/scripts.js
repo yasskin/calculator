805fe2542022859a96d3c69d0fe403131e90d156
Alex Jacks and Noah Yasskin <noah@>
2016-05-31 10:22:53 -0700

    add initial project files for JS calculator

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//Simple calculator code, addition and then subtraction of 2 input values//
+var Noah = function(num1, num2) {
+	return num1 + num2;
+};
+
+var num1 = parseInt(prompt("Gimme a number:"));
+var num2 = parseInt(prompt("Gimme another number!"));
+
+alert(Noah(num1, num2));
