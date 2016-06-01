b9f28b1fe522810766da08e62101bf72269890b1
Joel Delight and Noah Yasskin <noah@>
2016-06-01 10:25:44 -0700

    add html for an add function and styled the input forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,20 @@
-//Simple calculator code, addition and then subtraction of 2 input values//
-
-$(document).ready(function() {
-  $("button#green").click(function() {
-		$("body").removeClass();
-		$("body").addClass("green-background");
-  });
-
-  $("button#yellow").click(function() {
-		$("body").removeClass();
-    $("body").addClass("yellow-background");
-  });
-
-  $("button#red").click(function() {
-		$("body").removeClass();
-    $("body").addClass("red-background");
-  });
-});
-
-
-
-/* Base calc code
-var Adam = function(num1, num2) {
+var add = function(num1, num2) {
 	return num1 + num2;
 };
 
-var Sunshine = function(num1, num2) {
+var subtract = function(num1, num2) {
 	return num1 - num2;
 };
 
-var Mike = function(num1, num2) {
+var multiply = function(num1, num2) {
 	return num1 * num2;
 };
 
-var Dave = function(num1, num2) {
+var divide = function(num1, num2) {
 	return num1 / num2;
 };
 
 var num1 = parseInt(prompt("Gimme a number:"));
 var num2 = parseInt(prompt("Gimme another number!"));
 
-alert(Adam(num1, num2));
-alert(Sunshine(num1, num2));
-alert(Mike(num1, num2));
-alert(Dave(num1, num2));
-*/
+alert(add(num1, num2));
