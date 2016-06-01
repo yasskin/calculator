97e15830aed97ff2ec7cfd63f6c89956f383d6bf
Joel Delight and Noah Yasskin <joel@>
2016-06-01 11:44:18 -0700

    add forms and functionality for multiplication, division, and subtraction

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business or back-end logic
+
 var add = function(num1, num2) {
 	return num1 + num2;
 };
@@ -14,7 +16,35 @@ var divide = function(num1, num2) {
 	return num1 / num2;
 };
 
-var num1 = parseInt(prompt("Gimme a number:"));
-var num2 = parseInt(prompt("Gimme another number!"));
+// Everything below this line is user interface logic
 
-alert(add(num1, num2));
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var num1 = parseFloat($("#add1").val());
+    var num2 = parseFloat($("#add2").val());
+    var result = add(num1, num2);
+    $("#output").text(result);
+  })
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+		var num1 = parseFloat($("#subtract1").val());
+		var num2 = parseFloat($("#subtract2").val());
+		var result = subtract(num1, num2);
+		$("#output").text(result);
+	})
+	$("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var num1 = parseFloat($("#multiply1").val());
+    var num2 = parseFloat($("#multiply2").val());
+    var result = multiply(num1, num2);
+    $("#output").text(result);
+  })
+	$("form#divide").submit(function(event) {
+		event.preventDefault();
+		var num1 = parseFloat($("#divide1").val());
+		var num2 = parseFloat($("#divide2").val());
+		var result = divide(num1, num2);
+		$("#output").text(result);
+	})
+})
